9eb5e8e614cdd86fb4fe17792051d457be233f38
werehampster <alex_c_stahl@hotmail.com>
2023-03-05 14:21:12 +0100

    imported React libs and created index.html

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// 1) Import the React and ReactDOM libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to the div with ID root
+
+// 3) Tell React to take control of the element
+
+// 4) Create a component
+
+// 5) Show the component on the screen
